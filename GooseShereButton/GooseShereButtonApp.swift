b360e44250dde90c6e0fd82008f3918a63cbb80f
Mykola Matsko <mykola.matsko@gmail.com>
2023-05-22 10:07:02 +0300

    Initial Commit

---
+++ GooseShereButton/GooseShereButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooseShereButtonApp.swift
+//  GooseShereButton
+//
+//  Created by Mykola Matsko on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GooseShereButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
